a73fb0146cd9d400d9a9c4c0113733822dd7e2ca
sushxworkz <sushmitahiragappagol@gmail.com>
2024-07-03 23:03:35 +0530

    03-07-2024

---
+++ Projector.java
@@ -0,0 +1,10 @@
+class Projector
+{
+	public static void main(String[] epson)
+	{
+		System.out.println("Projector are used very collage");
+		System.out.println("XGA Resolution");
+		System.out.println("3300 Lumens in Colour Brightness & White Brightness)");
+		System.out.println("Lamp Life 12000 Hrs (Eco Mode)");
+	}
+}

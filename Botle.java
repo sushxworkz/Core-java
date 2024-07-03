550d3719f64ad204f1a6269c3fc8482907e5adc4
sushxworkz <sushmitahiragappagol@gmail.com>
2024-07-03 11:08:42 +0530

    03-07-2024

---
+++ Botle.java
@@ -0,0 +1,11 @@
+class Botle{
+
+	public static void main(String[] milton){
+	
+		System.out.println("Color: Dark Blue; Material: Stainless Steel; Package Content: 1 - Piece Aura Thermosteel Bottle (1.05 Litre); Dimension in cms (L x W x H): 8.4 x 8.4 x 30.");
+		System.out.println("Insulated bottle that regulates temperature and keeps the drink cold for hours");
+		System.out.println("Double walled Vacuum insulated technology keeps beverages hot or cold for 24 hours, Inner copper coating for better temperature retention. A unique bottle that makes pouring completely hassle-free and spill-free");
+		System.out.println("This Water Bottle made from stainless steel, which makes it completely Safe to Store your water & beverages for the long time");
+		System.out.println("SS 304 Inside and outside makes this bottle rust proof, which makes it durable also. This bottle is easy to carry also as it comes with carry string for easy carrying. It can perfectly fit in side pocket of backpack");
+	}
+}

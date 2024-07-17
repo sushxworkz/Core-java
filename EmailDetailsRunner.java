ac59bf81c0aa39c517b299238157e82d2699480f
sushxworkz <sushmitahiragappagol@gmail.com>
2024-07-17 23:02:10 +0530

    17-07-2024

---
+++ EmailDetailsRunner.java
@@ -0,0 +1,13 @@
+class EmailDetailsRunner{
+
+	public static void main(String args[]){
+	
+		System.out.println("Main Started");
+			String inputEmail="sneha56@gmail.com";
+			EmailDetails.getEmailDetails(inputEmail);
+		System.out.println("Main Ended");
+		
+	}
+}
+	
+	

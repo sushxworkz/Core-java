3e1696a751cefaa06a7a2312e26d261dc9a1fba4
sushxworkz <sushmitahiragappagol@gmail.com>
2024-07-31 00:34:41 +0530

    30-07-2024

---
+++ Bill.java
@@ -0,0 +1,16 @@
+class Bill{
+
+	 int billReferenceId;
+	 int billSequenceNo;
+	 String billIssueDate;
+	 String billDueDate;
+	 boolean isBillPaid;
+	 boolean isLatestBill;
+	 boolean isFinalBill;
+	 double billAmount;
+	 boolean taxtInclusiveBill;
+	 
+	 //behavoiur
+	 
+}
+	
---
+++ BillRunner.java
@@ -0,0 +1,319 @@
+class BillRunner{
+
+	public static void main(String bill[]){
+	
+		//ClassName ref=new ClassName();
+		
+		Bill ref=new Bill();
+		
+		int billReferenceId=ref.billReferenceId;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=500;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=501;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=503;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=504;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=505;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=506;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=507;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=508;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=509;
+		System.out.println(billReferenceId);
+		
+		billReferenceId=510;
+		System.out.println(billReferenceId);
+		
+		int billSequenceNo=ref.billSequenceNo;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=1;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=2;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=3;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=4;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=5;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=6;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=7;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=8;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=9;
+		System.out.println(billSequenceNo);
+		
+		billSequenceNo=10;
+		System.out.println(billSequenceNo);
+		
+		String billIssueDate=ref.billIssueDate;
+		System.out.println(billIssueDate);
+		
+		billIssueDate="04-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="05-05-2024";
+		System.out.println(billIssueDate);
+			
+		billIssueDate="06-05-2024";
+		System.out.println(billIssueDate);
+			
+		billIssueDate="07-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="08-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="09-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="10-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="11-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="12-05-2024";
+		System.out.println(billIssueDate);
+		
+		billIssueDate="13-05-2024";
+		System.out.println(billIssueDate);
+		
+		String billDueDate=ref.billDueDate;
+		System.out.println(billDueDate);
+		
+		billDueDate="01-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="02-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="03-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="04-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="05-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="06-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="07-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="08-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="09-06-2024";
+		System.out.println(billDueDate);
+		
+		billDueDate="10-06-2024";
+		System.out.println(billDueDate);
+		
+		boolean isBillPaid=ref.isBillPaid;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=true;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=false;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=true;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=true;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=false;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=true;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=true;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=true;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=false;
+		System.out.println(isBillPaid);
+		
+		isBillPaid=false;
+		System.out.println(isBillPaid);
+		
+		boolean isLatestBill=ref.isLatestBill;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=true;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=false;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=true;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=true;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=false;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=false;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=false;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=true;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=false;
+		System.out.println(isLatestBill);
+		
+		isLatestBill=true;
+		System.out.println(isLatestBill);
+		
+		boolean  isFinalBill=ref. isFinalBill;
+		System.out.println( isFinalBill);
+		
+		isFinalBill=true;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=false;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=false;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=false;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=true;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=true;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=true;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=false;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=true;
+		System.out.println(isFinalBill);
+		
+		isFinalBill=false;
+		System.out.println(isFinalBill);
+		
+		double  billAmount=ref. billAmount;
+		System.out.println( billAmount);
+		
+		 billAmount=200.02;
+		System.out.println(billAmount);
+		
+		 billAmount=230.09;
+		System.out.println(billAmount);
+		
+		 billAmount=400.62;
+		System.out.println(billAmount);
+		
+		 billAmount=890.02;
+		System.out.println(billAmount);
+		
+		 billAmount=789.56;
+		System.out.println(billAmount);
+		
+		 billAmount=453.09;
+		System.out.println(billAmount);
+		
+		 billAmount=360.07;
+		System.out.println(billAmount);
+		
+		 billAmount=278.03;
+		System.out.println(billAmount);
+		
+		 billAmount=890.05;
+		System.out.println(billAmount);
+		
+		 billAmount=250.00;
+		System.out.println(billAmount);
+		
+		boolean taxtInclusiveBill=ref.taxtInclusiveBill;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=true;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=false;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=true;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=false;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=true;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=true;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=false;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=true;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=true;
+		System.out.println(taxtInclusiveBill);
+		
+		taxtInclusiveBill=false;
+		System.out.println(taxtInclusiveBill);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+		
+		
